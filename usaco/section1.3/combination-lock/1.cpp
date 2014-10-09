aa13646deb5ea73aa69f8b8c9aff4d7c5b7b1f85
alirezaomidi <alireza530@gmail.com>
2014-10-09 19:06:30 +0330

    new prob solved

--- usaco/section1.3/combination-lock/1.cpp
+++ usaco/section1.3/combination-lock/1.cpp
@@ -13,7 +13,7 @@ void gen(set<vector<int> >& combs, vector<int>& s, int& n, int level) {
 	static vector<int> comb(s);
 	if (level == 3) {
 		if (combs.find(comb) != combs.end()) return;
-		combs.insert(comb);	return;
+		combs.insert(comb); return;
 	}
 	for (int i = -2; i <= 2; ++i) {
 		comb[level] = (s[level] + i) % n + ((s[level] + i) % n <= 0) * n;
@@ -32,7 +32,7 @@ int main() {
 	vector<int> master(3);
 	for (int i = 0; i < 3; ++i) fin >> master[i];
 	set<vector<int> > combs;
-	if (n <= 5)	fout << n * n * n << endl;
+	if (n <= 5) fout << n * n * n << endl;
 	else {
 		gen(combs, fj, n, 0);
 		gen(combs, master, n, 0);

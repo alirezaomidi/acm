9ee2de76956019a5d28348a5b84f432de293b9b7
alirezaomidi <alireza530@gmail.com>
2015-07-18 00:53:23 +0430

    changed usaco kimbits

--- usaco/section3.2/stringsobits/1.cpp
+++ usaco/section3.2/stringsobits/1.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 typedef unsigned int ui;
 typedef vector<ui> vi;
-typedef vector<vi> vii;
+typedef vector<vi> vvi;
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -20,13 +20,13 @@ int main() {
 	ui n, len, order;
 	cin >> n >> len >> order;
 
-	vii comb(n + 1, vi(len + 1, 1));
+	vvi dp(n + 1, vi(len + 1, 1));
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= len; j++)
-			comb[i][j] = comb[i - 1][j - 1] + comb[i - 1][j];
+			dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
 
 	while (n--) {
-		ui c = comb[n][len];
+		ui c = dp[n][len];
 		if (order > c) {
 			cout << '1';
 			order -= c;

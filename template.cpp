2380ce75b031b557f6bb372317d7e798b7adfb92
alirezaomidi <alireza530@gmail.com>
2015-07-27 11:00:28 +0430

    added template

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<vii> vvii;
+typedef set<int> si;
+typedef vector<si> vsi;
+
+const int inf = 1e9;
+
+int main() {
+	ios::sync_with_stdio(false);
+
+
+
+	return 0;
+}

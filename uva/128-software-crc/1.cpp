ea583161893b2413e6c4717402816d9f592042fa
alirezaomidi <alireza530@gmail.com>
2015-08-07 13:56:26 +0430

    added uva 128: big mod

--- uva/128-software-crc/1.cpp
+++ uva/128-software-crc/1.cpp
@@ -25,9 +25,30 @@ typedef vector<si> vsi;
 const int inf = 1e9;
 
 int main() {
-	ios::sync_with_stdio(false);
+	// ios::sync_with_stdio(false);
 
+	string m;
+	while (getline(cin, m)) {
+		if (m == "#")
+			break;
+		int crc = 0, g = 34943;
+		for (int i = 0; i < m.size(); i++) {
+			crc <<= 8;
+			(crc += m[i]) %= g;
+		}
+		crc = ((ll)crc << 16) % g;
+		crc = (g - crc) % g;
 
+		// C++ Style
+		// cout << hex << uppercase << setfill('0')
+		// << setw(2) << (crc >> 8)
+		// << ' '
+		// << setw(2) << (crc % 256)
+		// << endl;
+
+		// C Style (much more better!)
+		printf("%02X %02X\n", crc >> 8, crc % 256);
+	}
 
 	return 0;
 }

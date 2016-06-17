79333540fb5af305a7c61abd724d92fbd3ca143b
alirezaomidi <alireza530@gmail.com>
2016-06-17 17:50:48 +0430

    Solve UVa 10055: ad-hoc

---
+++ uva/10055-heshmat-the-brave-warrior/1.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+typedef vector<vii> vvii;
+typedef set<int> si;
+typedef vector<si> vsi;
+typedef pair<double, double> dd;
+
+const int inf = 1e9;
+
+int main() {
+	ios::sync_with_stdio(false);
+
+	ll a, b;
+	while (cin >> a >> b) {
+		cout << abs(a - b) << endl;
+	}
+
+	return 0;
+}

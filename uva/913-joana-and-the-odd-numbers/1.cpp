84176872ff36dd56d4ce627465740225bae87b7a
alirezaomidi <alireza530@gmail.com>
2016-06-17 17:50:17 +0430

    Solve UVa 913: ad-hoc

---
+++ uva/913-joana-and-the-odd-numbers/1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+typedef vector<vii> vvii;
+typedef set<int> si;
+typedef vector<si> vsi;
+typedef pair<double, double> dd;
+
+const int inf = 1e9;
+
+int main() {
+	ios::sync_with_stdio(false);
+
+	ll n;
+	while (cin >> n) {
+		n = n / 2 + 1;
+		cout << 3 * (2 * n * n - 3) << endl;
+	}
+
+	return 0;
+}
